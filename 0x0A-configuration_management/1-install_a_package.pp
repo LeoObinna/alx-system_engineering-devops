825d4075fa22be91476250e75204383179cf6fe7
LeoObinna <leoaniude20@gmail.com>
2024-03-22 13:06:35 -0700

    Configuration management process

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 #!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+# Install a specific version of Flask (2.1.0)
+package {'Flask':
   ensure   => '2.1.0',
   provider => 'pip3'
+}
+
+# Install a specific version of Werkzeug (2.1.1)
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
