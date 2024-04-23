375c3cdc7e6e99378b2bb3ed45ab40a8a77c8bd3
LeoObinna <leoaniude20@gmail.com>
2024-04-22 17:38:21 -0700

    Using what you did in the task #0, extend your Python script to export data in the JSON format.

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This Exports a to-do list information for a given employee ID to JSON format."""
+"""Exports a to-do list information for a given employee ID to JSON format."""
 import json
 import requests
 import sys
